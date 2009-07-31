423e57d0ff5870bcddcc795da4d0555daf9ee9a7
yegle <me@yegle.net>
2009-07-31 15:05:36 +0000

    finally! now returns the proper http header.
    M    trunk/index.php

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 	$webroot = '/twip/trunk'; // where you put your twip index.php file.
 	$twitter = 'http://twitter.com'; //the upper api address. you can set this to another api proxy.
 	$logfile = 'log.txt';
-	//$authlist=array(
-	//	'account/verify_credentials\.(json|xml)',
-	//);
+
+
 	$request = array();
 	$request['url'] = substr( $_SERVER['REQUEST_URI'] , -strlen($_SERVER['REQUEST_URI']) + strlen($webroot) );
 	$request['method'] = ( $_POST==false ) ? 'get' : 'post';
@@ -18,14 +17,20 @@
 		$curlopts[CURLOPT_POST] = true;
 		$curlopts[CURLOPT_POSTFIELDS] = $_POST;
 	}
-	//$curlopts[CURLOPT_RETURNTRANSFER] = true;
+	$curlopts[CURLOPT_RETURNTRANSFER] = true;
 	$curlopts[CURLOPT_HTTPHEADER] = array('Expect:');
-	//$curlopts[CURLOPT_HEADER] = true;
+	$curlopts[CURLOPT_HEADER] = true;
 	curl_setopt_array($ch,$curlopts);
-	//$ret = curl_exec($ch);
+	$ret = curl_exec($ch);
 	curl_exec($ch);
 	$log = date('YmdHis').' '.$_SERVER['REMOTE_ADDR'].' '.$request['url'].' '.$request['method'].' '.$isauth."\n";
 	file_put_contents($logfile,$log,FILE_APPEND);
+	$headerlen = curl_getinfo( $ch,CURLINFO_HEADER_SIZE );
+	$header = substr($ret,0,$headerlen);
+	$headerarr = explode('<br />',nl2br($header));
+	foreach($headerarr as $item){
+		header(trim($item));
+	}
+	echo substr($ret,-strlen($ret) + $headerlen - 1 );
 	//file_put_contents('ret',$ret);
-	//echo $ret;
 ?>
