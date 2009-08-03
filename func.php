74ad6460dec4e8f3aeda08d293e2347009ebbcfd
yegle <me@yegle.net>
2009-08-03 10:26:36 +0000

    Fixed a BIG problem...that twip cannot @ somebody...
    Why I didn't found this bug...faint...
    
    AM   trunk/default_profile_normal.png
    M    trunk/func.php
    M    trunk/index.php

--- func.php
+++ func.php
@@ -3,11 +3,16 @@
 		header($str);
 		return strlen($str);
 	}
-	function dolog(){
+	function dolog($str = ''){
+		global $logfile;
 		global $requesturl;
 		global $method;
 		global $isauth;
-		$log = date('Y-m-d H:i:s').' '.$_SERVER['REMOTE_ADDR'].' '.$requesturl.' '.$method.' '.$isauth."\n";
+		$log = date('Y-m-d H:i:s').' '.$_SERVER['REMOTE_ADDR'].' '.$requesturl.' '.$method.' '.$isauth.' '.$str."\n";
 		file_put_contents($logfile,$log,FILE_APPEND);
+	}
+	function isSSL(){
+		if($_SERVER['HTTPS'] == 1 || $_SERVER['HTTPS'] == 'on') return true;
+		else return false;
 	}
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,33 @@
 <?php
-	require('func.php');
 	require('config.php');
+	require('func.php');
 
-
+	if(isSSL()){
+		$apiurl = 'https://'.$_SERVER['HTTP_HOST'].$webroot.'/';
+	}
+	else{
+		$apiurl = 'http://'.$_SERVER['HTTP_HOST'].$webroot.'/';
+	}
 	$requesturl = substr( $_SERVER['REQUEST_URI'] , -strlen($_SERVER['REQUEST_URI']) + strlen($webroot) );
+	if($requesturl == '/'){
+		echo '<html><head><title>Twip,Twitter API proxy in PHP.</title></head><body><h1>Twip,Twitter API proxy in PHP.</h1><p>This is a Twitter API proxy,and is not intend to be viewed in a browser.<br />Please use '.$apiurl.' in your Twitter Client.<br />Visit <a href="http://code.google.com/p/twip/">Twip </a> for more details.</p></body></html>';
+		exit();
+	}
+
+	//fixme: this is ugly...but it works...
+	//if you have any good ideas,tell me~
+	$type = 'json';
+	if( strpos($requesturl,'.xml') !== false ){
+		$type='xml';
+	}
+	else if( strpos($requesturl,'.json') !== false ){
+		$type='json';
+	}
+	else{
+		//since We only need to make twitter client to work
+		header($_SERVER["SERVER_PROTOCOL"]." 501 Not Implemented");	
+		exit();
+	}
 	$method = $_SERVER['REQUEST_METHOD'];
 	$ch = curl_init($twitter.$requesturl);
 
@@ -19,11 +43,27 @@
 				$_POST[$key] = stripslashes($_POST[$key]);
 			}
 		}
-		$curlopts[CURLOPT_POSTFIELDS] = $_POST;
+		foreach($_POST as $key => $value){
+			$_POST[$key] = $key.'='.urlencode($value);
+		}
+		$curlopts[CURLOPT_POSTFIELDS] = implode('&',$_POST);
 	}
+	$curlopts[CURLOPT_RETURNTRANSFER] = true;
 	$curlopts[CURLOPT_HTTPHEADER] = array('Expect:');
 	$curlopts[CURLOPT_HEADERFUNCTION] = 'echoheader';
 	curl_setopt_array($ch,$curlopts);
-	curl_exec($ch);
+	$ret = curl_exec($ch);
+	if($type == 'json'){
+		$ret = str_replace(json_encode("http://static.twitter.com/images/default_profile_normal.png"),json_encode($apiurl."default_profile_normal.png"),$ret);
+	}
+	else if ($type == 'xml'){
+		$ret = str_replace('http://static.twitter.com/images/default_profile_normal.png',$apiurl.'default_profile_normal.png',$ret);
+	}
+	echo $ret;
+	file_put_contents('ret',$ret);
+	if( $ret === false ){
+		dolog(curl_error($ch));
+		exit();
+	}
 	dolog();
 ?>
