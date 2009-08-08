d6587ef3d948cd97ff5a5be9e8ba9491c8d16abb
xmxsuperstar <xmxsuperstar@gmail.com>
2009-08-08 11:10:05 +0000

    if no config.php provided by user, cp config-example.php as config.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+	if(!file_exists('config.php')) {
+		//if no config.php provided by user, use config-example.php
+		copy('config-example.php', 'config.php');
+	}
+
 	require('config.php');
 	require('func.php');
 
