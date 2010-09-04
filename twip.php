239e6e1b7af6fe4f914ca3b71a6b140f7810397c
yegle <me@yegle.net>
2010-09-04 16:18:30 +0000

    Code cleanup.May not work at all.
    M    twip.php

--- twip.php
+++ twip.php
@@ -11,19 +11,15 @@ class twip{
     public function twip($options = null){
         ob_start();
         $this->parse_variables($options);
-        file_put_contents('log',$this->request_uri."\n",FILE_APPEND);
-        if($this->request_uri == 'oauth/access_token'){
-            $this->echo_token();
-            exit();
+        if($this->mode=='t'){
+            $this->transparent_mode();
         }
-        if($this->request_uri == null){
-            echo 'click <a href="oauth.php">HERE</a> to get your API url';
-            exit();
+        else if($this->mode=='o'){
+            $this->override_mode();
         }
-        $this->do_request();
-        print_r($this);
         $str = ob_get_contents();
         file_put_contents('debug',$str);
+        file_put_contents('log',$this->request_uri."\n",FILE_APPEND);
     }
 
     private function echo_token(){
@@ -49,17 +45,11 @@ class twip{
         //parse $_SERVER
         $this->method = $_SERVER['REQUEST_METHOD'];
 
-        //something more
+
         $this->request_uri = $this->parse_request_uri();
-        //$request_headers = getallheaders();
-        //if(strpos($request_headers['Accept-Encoding'],'gzip')!==NULL){
-        //    $this->gzip = true;
-        //}
-        //foreach($request_headers as $key=>$value){
-        //    if($key!=='Accept-Encoding' && $key!=='Host'){
-        //        $this->request_headers[] = $key.': '.$value;
-        //    }
-        //}
+    }
+
+    private function override_mode(){
         $access_token = @file_get_contents('oauth/'.$this->username.'.'.$this->password);
         if($access_token === FALSE){
             echo 'You are not allowed to use this API proxy';
@@ -68,9 +58,14 @@ class twip{
         $access_token = unserialize($access_token);
         $this->access_token = $access_token;
 
-        //Should be a function,which fix client-specific problems.
-        if(strpos($this->request_uri,'trends') === 0){
-            $this->request_uri = '1/'.$this->request_uri;
+        $this->url_fixer();
+    }
+
+    private function url_fixer(){
+        if($_GET['source']=='Twitter for iPhone'){
+            if(strpos($this->request_uri,'trends') === 0){
+                $this->request_uri = '1/'.$this->request_uri;
+            }
         }
         if((strpos($this->request_uri,'search.') === 0)){
             $this->request_uri = $this->parent_search_api.$this->request_url;
@@ -80,37 +75,28 @@ class twip{
         }
     }
 
+
     private function parse_request_uri(){
         $full_request_uri = substr('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],strlen($this->base_url));
-        list($this->username,$this->password,$ret) = explode('/',$full_request_uri,3);
+        list($this->mode,$this->username,$this->password,$ret) = explode('/',$full_request_uri,4);
         return $ret;
     }
     private function do_request(){
+        if($this->request_uri == 'oauth/access_token'){
+            $this->echo_token();
+            exit();
+        }
+        if($this->request_uri == null){
+            echo 'click <a href="oauth.php">HERE</a> to get your API url';
+            exit();
+        }
         $this->connection = new TwitterOAuth($this->oauth_key, $this->oauth_secret, $this->access_token['oauth_token'], $this->access_token['oauth_token_secret']);
-        //if(strpos($this->request_uri,'.xml') !== false){
-        //    $this->connection->format = 'xml';
-        //    $this->request_uri = str_replace('.xml','',$this->request_uri);
-        //}else{
-        //    $this->request_uri = str_replace('.json','',$this->request_uri);//the default format is json
-        //}
         if($this->method=='POST'){
             echo $this->connection->post($this->request_uri,$_POST);
         }
         else{
             echo $this->connection->get($this->request_uri);
         }
-        //$this->url = $this->parent_api.$this->request_uri;
-        //$ch = curl_init($this->url);
-        //curl_setopt($ch,CURLOPT_HTTPHEADER,$this->request_headers);
-        //curl_setopt($ch,CURLOPT_HEADERFUNCTION,array($this,'headerfunction'));
-        //curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
-        //if($this->method=='POST'){
-        //    curl_setopt($ch,CURLOPT_POST,TRUE);
-        //    curl_setopt($ch,CURLOPT_POSTFIELDS,@file_get_contents('php://input'));
-        //    $this->post_fileds = @file_get_contents('php://input');
-        //}
-        //$this->response_body = curl_exec($ch);
-        ////$this->response_body . = 'oauth_token='.$this->access_token['oauth_token']."&oauth_token_secret=".$this->access_token['oauth_token_secret']."&user_id=".$this->access_token['user_id']."&screen_name=".$this->access_token['screen_name'].'&x_auth_expires=0';
     }
 
     private function headerfunction($ch,$str){
