1610f07201e55a852a80569c9977d50ae4ede0c0
yegle <me@yegle.net>
2010-09-04 09:16:12 +0000

    Updated the example file
    M    index-example.php

--- index-example.php
+++ index-example.php
@@ -2,5 +2,6 @@
 require('twip.php');
 $options['oauth_key'] = '7YyEToJbicfnTFVUNAl3yQ';
 $options['oauth_secret'] = 'cnJu23PWgiPKv4xFtGKNnTO5MqQafnwdd3zGIDtMA';
+$options['base_url'] = 'http://yegle.net/twip/';
 $twip = new twip($options);
 ?>
