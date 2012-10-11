36b6b43aa5411c7ef03dea08e1b8c277fce35dd8
yegle <me@yegle.net>
2012-10-10 23:01:04 -0400

    Update: version number in config-example.php

--- config-example.php
+++ config-example.php
@@ -2,7 +2,7 @@
 define('OAUTH_KEY','');
 define('OAUTH_SECRET','');
 define('BASE_URL','');
-define('API_VERSION','1');
+define('API_VERSION','1.1');
 define('DEBUG',FALSE);
 define('DOLOG',FALSE);
 define('COMPRESS',FALSE);
