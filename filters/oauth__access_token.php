f8923704e0bcf08583fe67617c3c9053e0a7ba12
Jimmy Xu <me@jimmyxu.org>
2014-04-01 08:55:21 +0000

    access_token: return json for consumers asking for that

--- filters/oauth__access_token.php
+++ filters/oauth__access_token.php
@@ -3,8 +3,14 @@
 $filterName = basename(__FILE__, '.php');
 
 $this->filters[$filterName] = function($args) {
+    if (@$_SERVER['HTTP_ACCEPT'] === 'application/json') {
+        $format = '{"oauth_token":"%s","oauth_token_secret":"%s","user_id":"%s","screen_name":"%s","x_auth_expires":0}';
+    }
+    else {
+        $format ="oauth_token=%s&oauth_token_secret=%s&user_id=%s&screen_name=%s&x_auth_expires=0\n";
+    }
     return sprintf(
-        "oauth_token=%s&oauth_token_secret=%s&user_id=%s&screen_name=%s&x_auth_expires=0\n",
+        $format,
         $args['self']->access_token['oauth_token'],
         $args['self']->access_token['oauth_token_secret'],
         $args['self']->access_token['user_id'],
